d6e4f27ab96448e634904ba13950b5fe9451a8e5
TrusTheDev <ivanbordeira2015@hotmail.com>
2025-03-03 15:26:15 -0300

    Basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainFormComponent } from "./main-form/main-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/main-form/main-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-form',
+  imports: [],
+  templateUrl: './main-form.component.html',
+  styleUrl: './main-form.component.css'
+})
+export class MainFormComponent {
+
+}
